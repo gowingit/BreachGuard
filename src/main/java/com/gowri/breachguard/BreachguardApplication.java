6edb55149df3a99afb101d3a94dbe90cb3cd4f73
gowingit <gowrimahesh1947@gmail.com>
2026-06-08 21:35:50 -0400

    Spring inital dependencies

---
+++ src/main/java/com/gowri/breachguard/BreachguardApplication.java
@@ -0,0 +1,13 @@
+package com.gowri.breachguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreachguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreachguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gowri/breachguard/BreachguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gowri.breachguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreachguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
